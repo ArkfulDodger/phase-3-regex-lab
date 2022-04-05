ef33af3958b1d87abf4aa44cc39771960add1cab
Noah Reece <noah.w.reece@gmail.com>
2022-04-05 14:02:44 -0400

    commit tree 687de530b65d6a5d4698e201d46844ec93fb6086

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,27 @@
-def starts_with_a_vowel?(word)
+require 'pry'
 
+def starts_with_a_vowel?(word)
+    # binding.pry
+    !!word.match(/\A[aeiouAEIOU]/)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+    text.scan(/\bun\w*ing\b/)
 end
+# binding.pry;
+
+
+/this/
+
 
 def words_five_letters_long(text)
-
+    text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+    !!text.match(/\A[A-Z].*\W\z/)
 end
 
 def valid_phone_number?(phone)
-
+    !!phone.match(/\A\(?[0-9]{3}\)?[-\s]?[0-9]{3}[-\s]?[0-9]{4}\z/)
 end
